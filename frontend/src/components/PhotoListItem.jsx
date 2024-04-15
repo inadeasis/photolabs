16b9900094044a734c2dad323ec8e1e6c837cc60
Ina Ricci <ina_ricci@hotmail.com>
2024-04-15 03:04:02 -0700

    modified styling to PhotoListItem

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -5,12 +5,16 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = ({photo}) => {
   /* Insert React */
 return (
-    <div className="photo-list__item">
-      <p>{photo.username}</p>
-      <img src={photo.imageSource} alt="photo" />
-      <p>{photo.id}</p>
-      <p>{photo.location.city}, {photo.location.country}</p>
-      <img src={photo.profile} alt="profile" />
+    <div className="photo-list__user-profile">
+      <div className="photo-list__user-info">
+        <p>{photo.username}</p>
+        <img src={photo.imageSource} alt="photo" />
+        <p>{photo.id}</p>
+      </div>
+
+    <p className="photo-list__location">{photo.location.city}, {photo.location.country}</p>
+
+    <img className="photo-list__image" src={photo.profile} alt="profile" />
     </div>  
   );
 };
