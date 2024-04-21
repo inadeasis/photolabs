caddaec6adce2532a643e375d311f8ed611bf776
Ina Ricci <ina_ricci@hotmail.com>
2024-04-21 00:54:46 -0700

    Added the Favourite Button

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,15 +4,20 @@ import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
+  const [favouritePhoto, setFavouritePhoto] = useState(false);
+
+  const handleClick = () => {
+    setFavouritePhoto(prev => !prev);
+  };
+
   return (
     <div className="photo-list__fav-icon">
       <div className="photo-list__fav-icon-svg">
-         <button className="LightSwitchButton">
-        {/* When the state is favourited */}<span className="fave"><FavIcon /></span>
-      </button>
+        <button className="LightSwitchButton" onClick={handleClick}>
+          <FavIcon isFavourite={favouritePhoto} />
+        </button>
       </div>
     </div>
   );
 }
-
 export default PhotoFavButton;
