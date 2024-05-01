6d81891278ae24d4642c77f018909d2bd86c9f29
Ina Ricci <ina_ricci@hotmail.com>
2024-05-01 11:31:28 -0700

    pass the photos prop to the PhotoList component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,32 +3,20 @@ import React from 'react';
 import PhotoList from 'components/PhotoList';
 import './App.scss';
 
-// const sampleDataForPhotoListItem = {
-//   id: "1",
-//   location: {
-//     city: "Montreal",
-//     country: "Canada",
-//   },
-//   imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-//   username: "Joe Example",
-//   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-// };
-
 // Note: Rendering a single component to build components in isolation
 const App = () => {
-  // const photos = [...Array(3)];
-  // const sampleArrayOfPhotos = photos.map((photo, i) => 
-  //   // <PhotoListItem key={i} photo={sampleDataForPhotoListItem} />
+  const photos = [...Array(3)];
+  const sampleArrayOfPhotos = photos.map((photo, i) => 
+      <PhotoList key={i} photo={sampleDataForPhotoListItem} />
     
     
-  // );
+   );
 
-  // return (
-  //   <div className="App">
-  //     {sampleArrayOfPhotos}
-      <PhotoList />
-  //   </div>
-  // );
+   return (
+    <div className="App">
+     {sampleArrayOfPhotos}
+   </div>
+   );
   
 }
 
