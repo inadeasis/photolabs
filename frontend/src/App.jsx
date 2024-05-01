5078abdfa8d7ca416839a6cd7e3a136794cb9cb9
Ina Ricci <ina_ricci@hotmail.com>
2024-05-01 11:11:27 -0700

    grouped PhotoListItem into PhotoList component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,36 @@
 import React from 'react';
 
 import PhotoListItem from './components/PhotoListItem';
+import PhotoList from 'components/PhotoList';
 import './App.scss';
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
+// const sampleDataForPhotoListItem = {
+//   id: "1",
+//   location: {
+//     city: "Montreal",
+//     country: "Canada",
+//   },
+//   imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+//   username: "Joe Example",
+//   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+// };
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
-  const photos = [...Array(3)];
-  const sampleArrayOfPhotos = photos.map((photo, i) => 
-    <PhotoListItem key={i} photo={sampleDataForPhotoListItem} />
+  // const photos = [...Array(3)];
+  // const sampleArrayOfPhotos = photos.map((photo, i) => 
+  //   // <PhotoListItem key={i} photo={sampleDataForPhotoListItem} />
     
-  );
+    
+  // );
 
-  return (
-    <div className="App">
-      {sampleArrayOfPhotos}
-    </div>
-  );
+  // return (
+  //   <div className="App">
+  //     {sampleArrayOfPhotos}
+      <PhotoList />
+  //   </div>
+  // );
+  
 }
 
 export default App;
