72b0006f0af8152384b2f968b2daa351905e876d
Ina Ricci <ina_ricci@hotmail.com>
2024-05-18 17:43:30 -0700

    cleaned up code

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -4,17 +4,17 @@ import "../styles/PhotoListItem.scss";
 import PhotoFavButton from "./PhotoFavButton"
 
 const PhotoListItem = (props) => {
-  const { urls, user, location } = props.photoData;
+  const { item } = props;
 
   return (
   <div className="photo-list__item">
     <PhotoFavButton />
-    <img className="photo-list__image" src={props.urls.regular} alt="photo" />
+    <img className="photo-list__image" src={item.urls.regular} alt="photo" />
     <div className="photo-list__user-details">
-      <img className="photo-list__user-profile" src={props.user.profile} alt="" />
+      <img className="photo-list__user-profile" src={item.user.profile} alt="" />
       <div>
-        <p className="photo-list__user-info">{props.user.username}</p>
-        <p className="photo-list__user-location">{props.location.city} {props.location.country}</p>
+        <p className="photo-list__user-info">{item.user.username}</p>
+        <p className="photo-list__user-location">{item.location.city} {item.location.country}</p>
       </div>  
   </div>
 </div>
