71929497f5b716a2be609d23dc990426627d1642
Ina Ricci <ina_ricci@hotmail.com>
2024-05-18 17:38:49 -0700

    cleaned up code

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,26 +1,6 @@
 import React from "react";
 import TopicListItem from './TopicListItem';
 import "../styles/TopicList.scss";
-import topics from "mocks/topics";
-import photos from "mocks/photos";
-
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
 
 const TopicList = (props) => {
   const { topics } = props
