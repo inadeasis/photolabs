2f49ba286f3c16f3b325f7a647cbbf2023cbcf0e
Ina Ricci <ina_ricci@hotmail.com>
2024-05-22 00:52:20 -0700

    modified to specify the expected props in the function signature for clarity

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
-const PhotoDetailsModal = (props) => {
-  const { closeDisplayModal } = props
+const PhotoDetailsModal = (closeDisplayModal) => {
   return (
     <div className="photo-details-modal">
     <button onClick={() => closeDisplayModal(false) } className="photo-details-modal__close-button"> <img src={closeSymbol} alt="close symbol" />
